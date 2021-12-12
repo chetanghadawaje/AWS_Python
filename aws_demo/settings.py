b81b6da784ec1c7da13f954ca204e9092d7096eb
chetanghadawaje <Pass@123>
2021-12-12 14:01:11 +0530

    CFG get using cfg file.

--- aws_demo/settings.py
+++ aws_demo/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
 from . import *
+from .cfg_params import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
